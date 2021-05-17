064c788e938d1b907d0757a3224c2a7b6219df57
thalles <thalles63@gmail.com>
2021-05-17 13:11:49 -0300

    modificado para testes na citta

--- server.js
+++ server.js
@@ -1,43 +1,99 @@
-const mongoose = require('mongoose');
-const Document = require('./Document');
-
-mongoose.connect(process.env.DB_URI, {
-   useNewUrlParser: true,
-   useUnifiedTopology: true,
-   useFindAndModify: false,
-   useCreateIndex: true
-})
-
-const io = require('socket.io')(process.env.PORT, {
+const io = require('socket.io')(process.env.PORT || 3000, {
    cors: {
-      origin: process.env.FRONT_URI,
+      origin: process.env.FRONT_URI || "http://localhost:4200",
       methods: ['GET', 'POST']
    }
 });
 
 const defaultValue = "";
+let vereadoresLogados = [];
+let pediramPalavra = [];
+let jaFalaram = [];
+let sessaoAtual = {
+   id: 1,
+   titulo: "SESSÃO ORDINÁRIA 0051/2021",
+   evento: "",
+   status: "",
+   espaco: "ORDEM DO DIA",
+   projeto: ""
+};
 
 io.on('connection', socket => {
+   
+   socket.on('get-vereador-status', (vereador) => {
+      let presente = (vereadoresLogados.indexOf(vereador+'') >= 0);
+      let pediuPalavra = (pediramPalavra.indexOf(vereador+'') >= 0);
+      let jaFalou = (jaFalaram.indexOf(vereador+'') >= 0);
 
-   socket.on('get-document', async documentId => {
-      const document = await findOrCreateDocument(documentId);
-      socket.join(documentId);
-      socket.emit('load-document', document.data);
+      socket.emit('status-vereador', {id: vereador, presente, voto: '', impedido: false, pediuPalavra, jaFalou});
+   });
 
-      socket.on('send-changes', delta => {
-         socket.broadcast.to(documentId).emit('receive-changes', delta);
-      });
+   socket.on('get-sessao-status', () => {
+      socket.emit('status-sessao', sessaoAtual);
+   });
 
-      socket.on('save-document', async data => {
-         await Document.findByIdAndUpdate(documentId, { data })
-      })
-   })
+   socket.on('init-resultado-votacao', () => {
+      socket.broadcast.emit('resultado-votacao', {action: 'START', status: 'APROVADO', favoraveis: 9, contrarios: 5, impedidos: 1, ausentes: 6, abstidos: 0, projeto: "Projeto de lei 0001/2021"});
+   });
+
+   socket.on('init-discussao', () => {
+      sessaoAtual.status = 'DISCUSSAO';
+      socket.broadcast.emit('inicia-discussao', {status: "DISCUSSAO"});
+   });
+
+   socket.on('pedido-palavra', (vereador) => {
+      
+      if (pediramPalavra.indexOf(vereador+'') < 0) {
+         pediramPalavra.push(vereador);
+      } 
+
+      socket.broadcast.emit('pediu-palavra', {id: vereador, pediuPalavra: true});
+   });
+
+   socket.on('concessao-palavra', (vereador) => {
+      
+      pediramPalavra.splice(pediramPalavra.indexOf(vereador+''), 1)
+      jaFalaram.push(vereador);
+
+      socket.broadcast.emit('conceder-palavra', {id: vereador, projeto: "SESSÃO ORDINÁRIA 0051/2021"});
+   });
+
+
+   socket.on('finish-resultado-votacao', () => {
+      socket.broadcast.emit('resultado-votacao', {action: 'FINISH'});
+   });
+   
+   socket.on('init-leitura', () => {
+      sessaoAtual.status = "LEITURA";
+      sessaoAtual.projeto = "Projeto de lei 0001/2021";
+      let retorno = {
+         status: sessaoAtual.status,
+         projeto: sessaoAtual.projeto
+      }
+
+      socket.broadcast.emit('inicia-leitura', retorno);
+   });
+
+   socket.on('set-presenca', (vereador, presente) => {
+      if (!!presente && vereadoresLogados.indexOf(vereador+'') < 0) {
+         vereadoresLogados.push(vereador);
+      } 
+
+      if (!presente && vereadoresLogados.indexOf(vereador) >= 0) {
+         vereadoresLogados.splice(vereadoresLogados.indexOf(vereador+''), 1)
+      }
+
+      socket.broadcast.emit('presenca', {id: vereador, presente});
+   });
+
+   socket.on('init-timer', () => {
+      socket.broadcast.emit('inicia-timer', {date: new Date().getTime(), duracao: '5:00'});
+   });
+
+   socket.on('ping', function() {
+      setTimeout(() => {
+         socket.emit('pong');
+      }, 2000)
+   });
+
 });
-
-async function findOrCreateDocument(id) {
-   if (id == null) return;
-
-   const document = await Document.findById(id);
-   if (document) return document
-   return await Document.create({ _id: id, data: defaultValue })
-}
